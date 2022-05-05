62a1299738be58f26f43bba41f51215f4b66011d
tadaitsanya <101752405+tadaitsanya@users.noreply.github.com>
2022-05-04 21:24:34 -0500

    Add files via upload

---
+++ JS-Arrays-Objects/ASchwartz_JS-Arrays-Objects.js
@@ -0,0 +1,38 @@
+// Exercise 7 //
+var fruitArray = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruitArray.pop();
+console.log(fruitArray);
+fruitArray.sort();
+fruitArray.push(Kiwi);
+console.log(fruitArray);
+fruitArray.delete(1);
+console.log(fruitArray);
+fruitArray.reverse();
+
+
+
+// Exercise 8 //
+const array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+let array = array2[1,1];
+
+
+
+// Exercise 9 //
+let userLoser = {
+    username: 'Anya123',
+    password: 'Password123',
+};
+let database = {userLoser};
+let newsfeed = {
+    nameOne: {
+        username: 'AveryThree',
+        timeline: '2 years'
+    },
+    twoName: {
+        username: 'TiffanyTwo',
+        timeline: '9 months'
+    },
+    threeName: {
+        username: 'JacksonOne',
+        timeline: '6 years'
+    }};
